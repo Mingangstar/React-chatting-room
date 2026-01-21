1cafdb1021e492555562ba2429a43758299a46a6
민경은(윈컴) <wrud3vk@lgupluspartners.co.kr>
2026-01-21 18:13:31 +0900

    Chat.js 수정 (버전 불일치)

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,57 +1,67 @@
 import React, { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import queryString from 'query-string'
 import io from 'socket.io-client'
 import './Chat.css'
-import InfoBar from '../InforBar/InfoBar';
+import InfoBar from '../InforBar/InfoBar'
 import Messages from '../Messages/Messages'
 import Input from '../Input/Input'
 import TextContainer from '../TextContainer/TextContainer'
+
 const ENDPOINT = 'http://localhost:5000'
 let socket
 
-const Chat = ({ location }) => {
+const Chat = () => {
+  const location = useLocation()
+
   const [name, setName] = useState('')
   const [room, setRoom] = useState('')
   const [users, setUsers] = useState('')
   const [message, setMessage] = useState('')
   const [messages, setMessages] = useState([])
+
   useEffect(() => {
     const { name, room } = queryString.parse(location.search)
+
     socket = io(ENDPOINT)
 
     setRoom(room)
     setName(name)
+
     socket.emit('join', { name, room }, (error) => {
-      if (error) {
-        alert(error)
-      }
+      if (error) alert(error)
     })
-  }, [ENDPOINT, location.search])
+
+    return () => {
+      socket.disconnect()
+      socket.off()
+    }
+  }, [location.search])
+
   useEffect(() => {
-    //로딩 될때만 실행
     socket.on('message', (message) => {
-      setMessage((message) => [...messages, message])
+      setMessages((messages) => [...messages, message])
     })
+
     socket.on('roomData', ({ users }) => {
       setUsers(users)
     })
   }, [])
+
   const sendMessage = (event) => {
     event.preventDefault()
+
     if (message) {
       socket.emit('sendMessage', message, () => setMessage(''))
     }
   }
+
   return (
     <div className='outerContainer'>
       <div className='container'>
         <InfoBar room={room} />
         <Messages messages={messages} name={name} />
-        <Input
-          message={message}
-          setMessage={setMessage}
-          sendMessage={sendMessage}
-        />
+        <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
       </div>
       <TextContainer users={users} />
     </div>
